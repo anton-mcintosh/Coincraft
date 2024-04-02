93c97cbfd6e8b39e5cb903ce04392420ee5c02d4
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-04-01 19:29:32 -0700

    Fixed buysell, added logic to start using macd strat

--- buysell.go
+++ buysell.go
@@ -1,13 +1,13 @@
 package main
 
-func buy(s.Money, quantity int, price float64) {
-  s.Dollars -= quantity * price
-  s.Stock += quantity
+func (s *Money) buy(quantity int, price float64) {
+  s.Dollars -= float64(quantity) * price
+  s.Coins += float64(quantity)
   s.Buys++
 }
 
-func sell(s.Money, quantity int, price float64) {
-  s.Dollars += quantity * price
-  s.Stock -= quantity
+func (s *Money) sell (quantity int, price float64) {
+  s.Dollars += float64(quantity) * price
+  s.Coins -= float64(quantity)
   s.Sells++
 }
--- main.go
+++ main.go
@@ -17,34 +17,43 @@ type Money struct {
 type CoinResponse struct {
   Prices [][]interface{}`json:"prices"`
 }
+var data CoinResponse
 
-func main() {
-
-  var coin string
-  
-  fmt.Println("Pick a coin")
-  fmt.Scanln(&coin)
-  url := "https://api.coingecko.com/api/v3/coins/" + coin + "/market_chart?vs_currency=usd&days=365"
-	req, _ := http.NewRequest("GET", url, nil)
-
-	res, _ := http.DefaultClient.Do(req)
-
-  var data CoinResponse
-  json.NewDecoder(res.Body).Decode(&data)
-
-  prices := extractPrices(data)
-  macd, signal := MACD(prices, 12, 26, 9)
-
-  for i := 0; i < len(macd); i++ {
-    fmt.Println(macd[i], "- - - ", signal[i])
-  }
-}
-
-func extractPrices(data CoinResponse) []float64 {
+func extractPrices (data CoinResponse) []float64 {
   var prices []float64
   for _, entry := range data.Prices {
     price := entry[1].(float64)
     prices = append(prices, price)
   }
   return prices
+}
+func main() {
+
+  money := Money{1000, 0, 0, 0}
+
+  var coin string
+  var strategy string
+  var trade_fee int
+  
+  fmt.Println("Pick a coin")
+  fmt.Scanln(&coin)
+  fmt.Println("Pick a strategy")
+  fmt.Scanln(&strategy)
+  fmt.Println("Specify a trade fee")
+  fmt.Scanln(&trade_fee)
+  url := "https://api.coingecko.com/api/v3/coins/" + coin + "/market_chart?vs_currency=usd&days=365"
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, _ := http.DefaultClient.Do(req)
+
+  json.NewDecoder(res.Body).Decode(&data)
+
+  prices := extractPrices(data)
+  macd, signal := MACD(prices, 12, 26, 9)
+
+  for i := 0; i < len(macd); i++ {
+    if macd[i] > signal[i] {
+      money.buy(1, prices[i])
+    }
+  }
 }
